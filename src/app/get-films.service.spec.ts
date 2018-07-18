ca230c70cc54c7c655bc1568195e9e601c02180d
Alex Novikov <alex@asnovikov.ru>
2018-07-17 21:09:08 -0300

    added service;

--- src/app/get-films.service.spec.ts
+++ src/app/get-films.service.spec.ts
@@ -1,11 +1,25 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { GetFilmsService } from './get-films.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
+
+let httpClientSpy: {
+  get: jasmine.Spy,
+  jsonp: jasmine.Spy,
+};
+
+// TODO: use HttpClientTestingModule
+
 
 describe('GetFilmsService', () => {
+
   beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'jsonp']);
     TestBed.configureTestingModule({
-      providers: [GetFilmsService]
+      providers: [GetFilmsService,
+        {provide: HttpClient, useValue: httpClientSpy}]
     });
   });
 
@@ -13,8 +27,20 @@ describe('GetFilmsService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('should be created', inject([GetFilmsService], (service: GetFilmsService) => {
-    expect(service).toBeTruthy();
+  it('should save film name', inject([GetFilmsService], (service: GetFilmsService) => {
+    service.getFilmsByName('qwerty');
+    console.log(service.name);
+    expect(service.name).toEqual('qwerty');
+  }));
+
+  it('should initiate a new call to the API', inject([GetFilmsService], (service: GetFilmsService) => {
+    service.getFilmsByName('qwerty');
+    console.log(service.name);
+    expect(service.name).toEqual('qwerty');
+
+    expect(httpClientSpy.jsonp.calls.count()).toEqual(1);
+    expect(httpClientSpy.jsonp.calls.mostRecent().args)
+      .toEqual([`${environment.apiUrl}?term=qwerty`, 'callback']);
   }));
 
 });
--- src/app/get-films.service.ts
+++ src/app/get-films.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Film } from '../models/film';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetFilmsService {
 
-  constructor() { }
+  name = '';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getFilmsByName(name: string): Observable<Film[]> {
+    this.name = name;
+    console.log(name);
+    return this.http.jsonp<Film[]>(`${environment.apiUrl}?term=${name}`, `callback`); // &callback=JSON_CALLBACK
+  }
+
 }
